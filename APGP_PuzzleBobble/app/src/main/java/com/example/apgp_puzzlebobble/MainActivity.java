801a35be8a9f1214a85a7460434df825c6f8509b
chodot <chohs0514@naver.com>
2023-04-14 14:14:50 +0900

    init project

---
+++ APGP_PuzzleBobble/app/src/main/java/com/example/apgp_puzzlebobble/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.apgp_puzzlebobble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
